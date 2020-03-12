87a1325c73bfef1acbabb0198425fd9fd27b1110
Leon Pu <puliwang@cxist.com>
2020-03-12 17:28:35 +0800

    添加css基础配置

---
+++ src/index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var btnTestNode = document.getElementById('btnTest');
+    btnTestNode.addEventListener('click', funTest);
+}
+
+function funTest() {
+    alert(1);
+}
