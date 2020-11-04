27fbdf06572e82126d925497082724828f024229
leon <puliwang@cxist.com>
2020-11-04 22:48:23 +0800

    添加monaco实施

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-window.onload = function () {
-    var btnTestNode = document.getElementById('btnTest');
-    btnTestNode.addEventListener('click', funTest);
-}
+require.config({ paths: { vs: './assets/vs' } });
 
-function funTest() {
-    console.log('test work!');
-}
+require(['vs/libs/jquery', 'vs/editor/editor.main'], function ($) {
+    var editorContainer = $('#editor-container')[0];
+    var editor = monaco.editor.create(editorContainer, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+        language: 'javascript'
+    });
+
+    monaco.languages.registerCompletionItemProvider('javascript', {
+        triggerCharacters: ["."],
+        autoIndent: true,
+        provideCompletionItems: function (model, position) {
+            // console.log(model, position);
+            var word = model.getWordUntilPosition(position);
+            var range = {
+                startLineNumber: position.lineNumber,
+                endLineNumber: position.lineNumber,
+                startColumn: word.startColumn,
+                endColumn: word.endColumn
+            };
+
+            // console.log(word);
+            // console.log(range);
+        }
+    });
+    // console.log(1, monaco);
+});
