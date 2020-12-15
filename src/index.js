455b7ad71545d297d2467d514beb820fa50637f4
Leon Pu <puliwang@cxist.com>
2020-12-15 16:57:13 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 window.onload = function () {
-    var btnTestNode = document.getElementById('btnTest');
-    btnTestNode.addEventListener('click', funTest);
+    var btn1 = document.getElementById('btn1');
+    var btn2 = document.getElementById('btn2');
+    btn1.addEventListener('click', fun1);
+    btn2.addEventListener('click', fun2);
 }
 
-function funTest() {
-    console.log('test work!');
+function fun1() {
+    task();
+    console.log('complete!');
+}
+
+function fun2() {
+    console.log('test 2 work!');
+}
+
+var i = 0;
+
+function task() {
+    var tip = document.getElementById('tip');
+    do {
+        i++;
+        tip.innerHTML = i;
+    } while (i < 1e3)
 }
